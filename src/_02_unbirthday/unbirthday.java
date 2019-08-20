538537c9cd38c49762ebb56e109c281b04fe216a
ilabstaff <ilabstaff@172.16.116.128>
2019-08-19 19:32:10 -0700

    unbirthday

---
+++ src/_02_unbirthday/unbirthday.java
@@ -0,0 +1,16 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+	public static void main(String[] args) {
+		
+	
+	String input=JOptionPane.showInputDialog("when is your birthday");
+			if(input.equals("08/19")) {
+				JOptionPane.showMessageDialog(null, "happy birthday");
+			}
+		
+			
+}
+}
