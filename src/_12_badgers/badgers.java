dab63424fc3e4bcda19920ca0a91210160763952
kaylarwright <kayla.wright@gompersstudent.org>
2019-09-16 19:05:57 -0700

    done

---
+++ src/_12_badgers/badgers.java
@@ -0,0 +1,28 @@
+package _12_badgers;
+
+
+public class badgers {
+public static void main(String[] args) {
+	
+	
+	for (int k = 0;  k< 3; k++) {
+		
+		
+	
+	for (int i = 0; i < 5; i++) {
+		System.out.println("badger");
+		}
+		for (int j = 0; j < 2; j++) {
+			System.out.println("mushroom");
+		}
+		
+	}	
+		
+		
+		for (int j = 0; j < 2; j++) {
+			System.out.println("snake");
+		}
+		
+	
+}
+}
